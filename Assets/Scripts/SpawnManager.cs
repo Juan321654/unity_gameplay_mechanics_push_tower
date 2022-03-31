27fab963d7bd6d97d81a2df10317e17976b7a57f
juan camacho <juancamachopers@hotmail.com>
2022-03-31 18:11:35 -0400

    done

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    private float spawnRange = 9.0f;
-    void Start()
-    {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-    }
+  public GameObject enemyPrefab;
+  private float spawnRange = 9.0f;
+  public int enemyCount;
+  public int waveNumber = 1;
+  public GameObject powerupPrefab;
+  void Start()
+  {
+    spawnEnemyWave(waveNumber);
+    spawnGemPowerUp();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    enemyCount = FindObjectsOfType<Enemy>().Length;
 
-    private Vector3 GenerateSpawnPosition()
+    if (enemyCount == 0)
     {
-        float randomNum = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(randomNum, 0, randomNum);
-        return randomPos;
+      waveNumber++;
+      spawnEnemyWave(waveNumber);
+      spawnGemPowerUp();
     }
+  }
+  void spawnEnemyWave(int enemiesToSpawn)
+  {
+    for (int i = 0; i < enemiesToSpawn; i++)
+    {
+      Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+  }
+
+  void spawnGemPowerUp()
+  {
+      Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+  }
+
+  private Vector3 GenerateSpawnPosition()
+  {
+    float randomNum = Random.Range(-spawnRange, spawnRange);
+    Vector3 randomPos = new Vector3(randomNum, 0, randomNum);
+    return randomPos;
+  }
 }
